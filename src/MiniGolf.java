c8a5e93e651cdc9baf69db56060956b4029a0735
hapsters <davidjhappe@gmail.com>
2015-05-26 15:35:05 -0400

    MiniGolf_Better
    Glorious JCreator and Java files

---
+++ src/MiniGolf.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)MiniGolf.java
+ *
+ * MiniGolf application
+ *
+ * @author 
+ * @version 1.00 2015/5/26
+ */
+ 
+public class MiniGolf {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
